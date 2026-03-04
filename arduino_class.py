beca27ff998f6177f2d22925184f09441e77678e
Igor Pitler <igor.pitler.work@gmail.com>
2026-03-04 17:25:37 +0700

    save

---
+++ arduino_class.py
@@ -0,0 +1,29 @@
+import serial
+
+class ArduinoDevice:
+
+    # real communication later
+    baud_rate = 9600
+    led_state="OFF"
+
+    def __init__(self, baud_rate: int =9600):
+        # some initial action
+        # real communication later
+        self.baud_rate=baud_rate
+        self.led_state="OFF"
+
+    def get_state(self) -> str:
+        return self.led_state
+
+    def set_led_on(self):
+        # add real communication later
+        self.led_state="ON"
+
+    def set_led_off(self):
+        # add real communication later
+        self.led_state="OFF"
+
+    def close(self):
+        # closing connection and stuff like that
+        pass
+
---
+++ tango_arduino.py
@@ -0,0 +1,31 @@
+import arduino_class
+print("tango!")
+
+# PyTango imports
+import tango
+from tango.server import run
+from tango.server import Device
+from tango.server import attribute, command
+from tango.server import class_property, device_property
+from tango import AttrQuality, DispLevel, DevState
+from tango import AttrWriteType
+# Additional import
+
+class TangoArduino(Device):
+
+
+    port_name=device_property(dtype=str)
+
+
+
+    @attribute(dtype=str)
+    def led_state(self):
+        return 'ON'
+
+    @command(dtype_out=str)
+    def set_led_on(self):
+        self.info_stream('Turning on the power supply')
+
+# test
+#myArduino=arduino_class.ArduinoDevice()
+#print("Arduino state: "+myArduino.get_state())

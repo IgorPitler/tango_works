e5c94fd8f061c46a79c76a5be40e5d9147c16464
Igor Pitler <igor.pitler.work@gmail.com>
2026-03-04 19:09:26 +0700

    save

--- tango_arduino.py
+++ tango_arduino.py
@@ -21,12 +21,32 @@ class TangoArduino(Device):
 
     @attribute(dtype=str)
     def led_state(self):
+        # temp
         return 'ON'
 
-    @command(dtype_out=str)
+    @command()
     def set_led_on(self):
-        self.info_stream('Turning on the power supply')
+        self.info_stream('Turning on the LED')
+
+    def set_led_off(self):
+        self.info_stream('Turning off the LED')
+
+    def init_device(self):
+        super().init_device() # call first
+        # runs on device initialization
+        pass # do something
+
+    def delete_device(self):
+        pass
+
+        super().delete_device() # call last
+
 
 # test
 #myArduino=arduino_class.ArduinoDevice()
 #print("Arduino state: "+myArduino.get_state())
+
+
+if __name__ == "__main__":
+    #TangoArduino.run_server()
+    pass

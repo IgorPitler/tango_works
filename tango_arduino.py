e818f55b6719ffe7eef641aaf7d931f1dfd11d05
Igor Pitler <igor.pitler.work@gmail.com>
2026-03-08 17:49:32 +0700

    save

--- tango_arduino.py
+++ tango_arduino.py
@@ -1,43 +1,52 @@
 #v1
+from os import access
 import arduino_class
-print("Tango!")
-
-# PyTango imports
+#print("Tango!")
 
 from tango.server import Device
 from tango.server import attribute, command
 from tango.server import device_property
 from tango import AttrQuality, DispLevel, DevState
 from tango import AttrWriteType
-# Additional import
+
 
 class tango_arduino(Device):
 
-    #arduino_device=arduino_class.ArduinoDevice()
-
-
     port_name=device_property(dtype=str)
-
-
 
     @attribute(dtype=str)
     def led_state(self):
-        # temp
-        arduino_state=self.arduino_device.get_led_state()
-        return arduino_state
+        return self.arduino_device.get_led_state()
 
-    # built-in attributes State and Status
+    #led_state=attribute(label="LED state attribute", dtype=str, fget="read_led_state", access=AttrWriteType.READ)
+
+    #@led_state.getter
+    #def read_led_state(self) -> str:
+    #    return self.arduino_device.get_led_state()
 
     @command()
     def set_led_on(self):
         self.arduino_device.set_led_on()
+
+        self.set_status("Status: LED is ON")
+        self.set_state(DevState.ON)
+
         print('Turning on the LED')
-        print("LED is "+self.arduino_device.get_led_state()+" Port is: "+self.arduino_device.port_name)
+        #print("LED is "+self.arduino_device.get_led_state()+" Port is: "+self.arduino_device.port_name+" Device status is "+self.get_status())
+        #print(self.get_state())
+        #print(self.led_state)
 
     @command()
     def set_led_off(self):
         self.arduino_device.set_led_off()
+
+        self.set_status("Status: LED is OFF")
+        self.set_state(DevState.OFF)
+
         print('Turning off the LED')
+        #print("LED is " + self.arduino_device.get_led_state() + " Port is: " + self.arduino_device.port_name+" Device status is "+self.get_status())
+        #print(self.get_state())
+        #print(self.led_state.read(fget=))
 
     def init_device(self):
         super().init_device() # call first
@@ -46,20 +55,14 @@ class tango_arduino(Device):
 
         self.arduino_device=arduino_class.ArduinoDevice(self.port_name, 9600)
 
-        #test!
-        self.set_state(DevState.ON)
-        self.set_status("Running OK")
+        self.set_state(DevState.INIT)
+        self.set_status("Status: Init")
 
     def delete_device(self):
         pass
         self.arduino_device.close()
         print("Deleting device...")
         super().delete_device() # call last
-
-
-# test
-#myArduino=arduino_class.ArduinoDevice()
-#print("Arduino state: "+myArduino.get_state())
 
 
 if __name__ == "__main__":

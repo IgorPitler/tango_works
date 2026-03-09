d71f10ab0e45d6f583cd28c9ee7a9e54995b5ff7
Igor Pitler <igor.pitler.work@gmail.com>
2026-03-09 12:15:31 +0700

    save

--- arduino_class.py
+++ arduino_class.py
@@ -1,52 +1,46 @@
-#v1
+# v1
 import serial
 import py_serial_lib
 
+
 class ArduinoDevice:
 
-    #port_name="/dev/ttyUSB0"
+    # port_name="/dev/ttyUSB0"
 
     baud_rate = 9600
-    led_state="OFF"
+    led_state = "OFF"
 
-    def __init__(self, port_name: str = "/dev/ttyUSB0", baud_rate: int = 9600, timeout: int = 1):
+    def __init__(
+        self, port_name: str = "/dev/ttyUSB0", baud_rate: int = 9600, timeout: int = 1
+    ):
         # some initial action
         self.real_arduino = py_serial_lib.SerialDevice(port_name, baud_rate, timeout)
         # Arduino tells "CONNECTED" on this real device
-        self.port_name=port_name
-        self.baud_rate=baud_rate
-        self.led_state="OFF"
+        self.port_name = port_name
+        self.baud_rate = baud_rate
+        self.led_state = "OFF"
 
     def get_led_state(self) -> str:
         self.real_arduino.send("GETSTATE.")
-        self.led_state=self.real_arduino.get_response()
+        self.led_state = self.real_arduino.get_response()
         # it has timeout on Arduino, need wait
         return self.led_state
 
     def set_led_on(self):
         self.real_arduino.send("ON.")
-        self.led_state="ON"
+        self.led_state = "ON"
 
     def set_led_off(self):
         self.real_arduino.send("OFF.")
-        self.led_state="OFF"
+        self.led_state = "OFF"
 
     def close(self):
         # closing connection and stuff like that
         self.real_arduino.close()
 
 
+# print('Error : '+arduino.get_error_description())
 
+# answer=arduino.get_response() # contains CONNECTED on my Arduino
 
-
-
-#print('Error : '+arduino.get_error_description())
-
-#answer=arduino.get_response() # contains CONNECTED on my Arduino
-
-#print(answer)
-
-
-
-
-
+# print(answer)
--- tango_arduino.py
+++ tango_arduino.py
@@ -1,4 +1,4 @@
-#v1
+# v1
 from os import access
 
 import arduino_class
@@ -7,20 +7,19 @@ from tango.server import Device
 from tango.server import attribute, command
 from tango.server import device_property
 from tango import AttrQuality, DispLevel, DevState
-from tango import AttrWriteType
 
 
 class tango_arduino(Device):
 
-    port_name=device_property(dtype=str)
+    port_name = device_property(dtype=str)
 
     @attribute(dtype=str)
     def led_state(self):
         return self.arduino_device.get_led_state()
 
-    #led_state=attribute(label="LED state attribute", dtype=str, fget="read_led_state", access=AttrWriteType.READ)
-    #@led_state.getter
-    #def read_led_state(self) -> str:
+    # led_state=attribute(label="LED state attribute", dtype=str, fget="read_led_state", access=AttrWriteType.READ)
+    # @led_state.getter
+    # def read_led_state(self) -> str:
     #    return self.arduino_device.get_led_state()
 
     @command()
@@ -28,27 +27,28 @@ class tango_arduino(Device):
         self.arduino_device.set_led_on()
         self.set_status("Status: LED is ON")
         self.set_state(DevState.ON)
-        print('Turning ON the LED')
+        print("Turning ON the LED")
 
     @command()
     def set_led_off(self):
         self.arduino_device.set_led_off()
         self.set_status("Status: LED is OFF")
         self.set_state(DevState.OFF)
-        print('Turning OFF the LED')
+        print("Turning OFF the LED")
 
     def init_device(self):
-        super().init_device() # call first
+        super().init_device()  # call first
         print("Starting our device...")
 
-        self.arduino_device=arduino_class.ArduinoDevice(self.port_name, 9600)
+        self.arduino_device = arduino_class.ArduinoDevice(self.port_name, 9600)
         self.set_state(DevState.INIT)
         self.set_status("Status: Init")
 
     def delete_device(self):
         self.arduino_device.close()
         print("Deleting device...")
-        super().delete_device() # call last
+        super().delete_device()  # call last
+
 
 if __name__ == "__main__":
     tango_arduino.run_server()

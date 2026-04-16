f8785bce2a753b985551c201fbb572fbf8e672f1
Igor Pitler <igor.pitler.work@gmail.com>
2026-04-16 17:22:11 +0700

    save

--- sample_macro1.py
+++ sample_macro1.py
@@ -5,6 +5,10 @@ from sardana.macroserver.msparameter import Type
 @macro([["String", Type.String, None, "this is par1 - string"],
        ["Float", Type.Float, None, "this is par2 - float"]]  )
 def sample_macro1(self, par1, par2):
+
+    # call other macro
+    self.wm("amot1")
+
     self.output(par1)
     self.output(par2)
     self.output("test ok!")

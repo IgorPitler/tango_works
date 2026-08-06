773d06d3f1482bb138a42c5221931d26a855eacb
Igor Pitler <igor.pitler.work@gmail.com>
2026-08-06 17:46:03 +0700

    save!

---
+++ linux_command.py
@@ -0,0 +1,10 @@
+import subprocess
+print("Run system command from Python:")
+
+res = subprocess.run(["ls", ], capture_output=True, text=True)
+print(res.stdout)
+with open("sample.sql", "r") as filehandle:
+    res = subprocess.run(["mariadb","-utango", "-ptango"], capture_output=True, stdin=filehandle, text=True)
+print(res.stdout)
+
+print("The end.")
